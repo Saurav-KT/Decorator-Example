ce412fbfc1328c86b17ae9f30048386720ee70d2
saurav j <saurav.saurav12@gmail.com>
2023-09-23 14:32:44 +0530

    Decorator with and without parameters example

--- decorator_example.py
+++ decorator_example.py
@@ -1,3 +1,8 @@
+"""Decorators without parameters example
+Example-1
+"""
+
+
 def make_preety(func):
     def inner():
         print("I got decorated")
@@ -11,17 +16,13 @@ def ordinary():
     print("I am ordinary")
 
 
-# call function
 # ordinary()
 
-# In the example shown below, make_pretty() is a decorator. In the assignment step. The function ordinary() got decorated and the returned function was given the name pretty.
-# preety = make_preety(ordinary)
-# preety()
+"""Problem Statement: Check the parameters type and passed value using decorator"""
 
 
-# Decorator with parameter example
 def check_param_type(func):
-    def validatedatatype(param1, param2):
+    def validatetype(param1, param2):
         if type(param1) is not int or type(param2) is not int:
             print("Invalid params")
             return False
@@ -30,11 +31,52 @@ def check_param_type(func):
         else:
             return func(param1, param2)
 
-    return validatedatatype
+    return validatetype
 
 
 @check_param_type
-def main_method(param1,param2):
-    print(param1,param2)
+def main_method(param1, param2):
+    print(param1, param2)
 
-# print(divide(10, 0))
+
+''' parametrized decorator example '''
+
+
+def print_in_loop(loop_repeate_count):
+    def middle_decorator(func):
+        def inner(msg: str):
+            for i in range(loop_repeate_count):
+                func(msg)
+
+        return inner
+
+    return middle_decorator
+
+
+@print_in_loop(4)
+def print_text(msg):
+    print(f"welcome to the world of python {msg}")
+
+
+print_text("Buddy")
+
+
+# Decorators with parameters Example-2
+def prefect_square(string_param):
+    def middle_decorator(func):
+        def inner(a: int):
+            print(f"{string_param} of {a}")
+            print(f"The square of {a} is")
+            return func(a)
+
+        return inner
+
+    return middle_decorator
+
+
+@prefect_square("find the square")
+def display_square(a):
+    return a * a
+
+
+print(display_square(5))
